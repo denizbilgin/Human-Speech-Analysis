9d00c09f8b4fbfdd24a7166120c6b3696214bd3a
denizbilgin4 <denizbilgin@posta.mu.edu.tr>
2024-03-24 21:09:46 +0300

    face detector system completed

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
 import cv2
-from ultralytics import YOLO
-import cvzone
 
 if __name__ == '__main__':
     # Video capturing with opencv
@@ -11,29 +8,38 @@ if __name__ == '__main__':
     image_bg = cv2.imread("recources/background.png")
     image_bg = cv2.resize(image_bg, (1280, 720))
 
-    # Creating instance of YOLO model
-    model = YOLO("yolo-weights/yolov8n.pt")
+    # Creating instance of face detector model
+    model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    num = 0
 
     while True:
         success, frame = cam.read()
 
-        # Applying the model and getting the results
-        results = model(frame, stream=True)
+        # Applying the cascade model and getting the results
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = model.detectMultiScale(gray, 1.3, 5)
 
-        # Looping through the results
-        for result in results:
-            boxes = result.boxes
-            for box in boxes:
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                w, h = x2 - x1, y2 - y1
+        # Looping through the faces
+        for face in faces:
+            x, y, w, h = face
 
-                cvzone.cornerRect(frame, (x1, y1, w, h))
-                confidence = math.ceil((box.conf[0]*100))/100
-                cvzone.putTextRect(frame, f"{confidence}", (x1, y1-20))
+            # Drawing border boxes
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 2)
 
-        #cv2.imshow("Webcam", frame)
+            # Getting roi of the frame
+            roi_gray = gray[y:y + h, x:x + w]
+
+            # Original colored frame
+            roi_color = frame[y:y + h, x:x + w]
+
         image_bg[116:116+480, 442:442+640] = frame
+
+        if num == 100:
+            cv2.imshow("100. frame budur", frame[y:y+h, x:x+w])
+            cv2.waitKey(0)
+        num += 1
+
         cv2.imshow("Background", image_bg)
 
         # Closing the camera
