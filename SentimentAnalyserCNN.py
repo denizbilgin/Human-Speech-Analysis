1c14cf3c81f0adf95fe7c39c7bc6251e8cb3d03b
denizbilgin4 <denizbilgin@posta.mu.edu.tr>
2024-04-01 01:10:20 +0300

    generator tests added

--- SentimentAnalyserCNN.py
+++ SentimentAnalyserCNN.py
@@ -1,6 +1,8 @@
+import numpy as np
 import tensorflow as tf
 import os
 from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     base_dir = "data/images/emotions"
@@ -10,7 +12,7 @@ if __name__ == '__main__':
     train_dir = os.path.join(base_dir, "train")
     test_dir = os.path.join(base_dir, "test")
 
-    train_datagen = ImageDataGenerator(rescale=1.0/255)
+    train_datagen = ImageDataGenerator(rescale=1.0/255, horizontal_flip=True)
     test_datagen = ImageDataGenerator(rescale=1.0/255)
 
     train_generator = train_datagen.flow_from_directory(
@@ -27,16 +29,12 @@ if __name__ == '__main__':
     )
 
     model = tf.keras.models.Sequential([
-        # Note the input shape is the desired size of the image 150x150 with 3 bytes color
         tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(48, 48, 3)),
         tf.keras.layers.MaxPooling2D(2, 2),
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
         tf.keras.layers.MaxPooling2D(2, 2),
-        # Flatten the results to feed into a DNN
         tf.keras.layers.Flatten(),
-        # 512 neuron hidden layer
         tf.keras.layers.Dense(512, activation='relu'),
-        # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('cats') and 1 for the other ('dogs')
         tf.keras.layers.Dense(7, activation='softmax')
     ])
 
@@ -48,3 +46,13 @@ if __name__ == '__main__':
         train_generator,
         epochs=15
     )
+
+    loss, acc = model.evaluate(test_generator)
+    print(acc)
+
+    # batch = next(test_generator)
+    # first_img = batch[0][0]
+    #
+    # plt.imshow(first_img)
+    # plt.axis('off')  # Eksenleri kapatın
+    # plt.show()
