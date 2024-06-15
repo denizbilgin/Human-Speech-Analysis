9c5e138f491e0d03f58c107b5b5c705d8fd89031
denizbilgin4 <denizbilgin@posta.mu.edu.tr>
2024-06-15 23:01:11 +0300

    new deep models trained

--- SentimentAnalyserCNN.py
+++ SentimentAnalyserCNN.py
@@ -3,31 +3,52 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import keras
+import seaborn as sns
+import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def plot_loss_accuracy(history, save_model):
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(12, 5))
 
+    # Model Accuracy
+    plt.subplot(1, 2, 1)
     plt.title("Model Accuracy")
     plt.plot(history.history["accuracy"])
     plt.plot(history.history["val_accuracy"])
-    plt.ylabel("accuracy")
-    plt.xlabel("epoch")
-    plt.legend(["train", "val"], loc="upper left")
-    if save_model:
-        plt.savefig("acc-epoch.png", dpi=300)
-    plt.show()
+    plt.ylabel("Accuracy")
+    plt.xlabel("Epoch")
+    plt.legend(["Train", "Val"], loc="upper left")
 
+    # Model Loss
+    plt.subplot(1, 2, 2)
     plt.title("Model Loss")
     plt.plot(history.history["loss"])
     plt.plot(history.history["val_loss"])
-    plt.ylabel("loss")
-    plt.xlabel("epoch")
-    plt.legend(["train", "val"], loc="upper left")
+    plt.ylabel("Loss")
+    plt.xlabel("Epoch")
+    plt.legend(["Train", "Val"], loc="upper left")
+
+    # Save the figure if required
     if save_model:
-        plt.savefig("loss-epoch.png", dpi=300)
+        final_f1_score = history.history["val_f1"][-1]
+        file_name = f"metrics_epoch_f1_{final_f1_score:.2f}.png"
+        plt.savefig(file_name, dpi=300)
+
+    plt.tight_layout()
     plt.show()
 
+def plot_confusion_matrix(model, generator):
+    y_val_pred = model.predict(generator, steps=generator.samples // generator.batch_size + 1)
+    y_val_pred_classes = np.argmax(y_val_pred, axis=1)
+    y_val_true = generator.classes
+    cm = tf.math.confusion_matrix(y_val_true, y_val_pred_classes)
+    class_names = list(generator.class_indices.keys())
+    plt.figure(figsize=(14, 9))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=class_names, yticklabels=class_names)
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.title('Confusion Matrix')
+    plt.show()
 
 class SentimentAnalyserCallback(keras.callbacks.Callback):
     def __init__(self, save_model):
@@ -39,8 +60,9 @@ class SentimentAnalyserCallback(keras.callbacks.Callback):
         self.start_time = time.time()
 
     def on_epoch_end(self, epoch, logs=None):
-        if logs is not None and logs.get("accuracy") > 0.99:
-            print(f"\nReached 99% accuracy, training stopped..")
+        mean_f1 = tf.reduce_mean(logs.get("val_f1_score")).numpy()
+        if logs is not None and mean_f1 > 0.85:
+            print(f"\nReached 85% F1 Score, training stopped..")
             self.model.stop_training = True
 
     def on_train_end(self, logs=None):
@@ -52,32 +74,33 @@ class SentimentAnalyserCallback(keras.callbacks.Callback):
         message = f"Training elapsed time was {str(hours)} hours, {minutes:4.1f} minutes, {seconds:4.2f} seconds."
         print(message)
 
+        last_val_f1 = tf.reduce_mean(logs.get("val_f1_score")).numpy()
         if logs is not None and self.save_model:
-            self.model.save(f"models/sentiment_analyser_CNN{logs.get('accuracy')*100:.2f}Acc.h5")
+            self.model.save(f"models/sentiment_analyser_CNN{last_val_f1*100:.2f}F1.h5")
             print("The model is successfully saved.")
 
-
 if __name__ == '__main__':
-
+    # General constants
     BASE_DIR = "data/images/emotions/"
     EMOTIONS = os.listdir(BASE_DIR)
     NUM_CLASSES = len(EMOTIONS)
     IMAGE_SIZE = (96, 96)
     BATCH_SIZE = 16
-    EPOCHS = 300
+    EPOCHS = 50
     SAVE_MODEL = True
-    SHOW_TRAINING_STATISTICS = True
     USE_SAVED_MODEL = False
+    SHOW_TRAINING_STATISTICS = not USE_SAVED_MODEL
     COLOR_MODE = "rgb"
 
     print(f"Contents of train dir: {EMOTIONS}")
+    print("-------------------------------")
 
+    # Creating train and validation datasets
     datagen = ImageDataGenerator(
         rescale=1.0/255,
         horizontal_flip=True,
         validation_split=0.2
     )
-
     train_generator = datagen.flow_from_directory(
         BASE_DIR,
         batch_size=BATCH_SIZE,
@@ -96,10 +119,21 @@ if __name__ == '__main__':
         subset="validation",
         color_mode=COLOR_MODE
     )
+    print("----------------------------------------")
+
+    # Checking whether the data is balanced or not
+    train_classes = train_generator.classes
+    unique_classes, class_counts = np.unique(train_classes, return_counts=True)
+    for cls, count in zip(unique_classes, class_counts):
+        print(f"There are {count} images for {EMOTIONS[cls]}.")
+    print("\nAs you can see the data is unbalanced.")
+    print("----------------------------------------")
 
     sample_batch = next(train_generator)
     print(f"Shape of sample batch is : {sample_batch[0].shape}")
+    print("----------------------------------------")
 
+    # Plotting the first image of the training generator to getting insight
     first_img = sample_batch[0][0]
     img_label = EMOTIONS[np.argmax(sample_batch[1][0])]
     plt.imshow(first_img)
@@ -110,44 +144,56 @@ if __name__ == '__main__':
     if USE_SAVED_MODEL:
         model = keras.models.load_model("models/sentiment_analyser_CNN63.52Acc.h5")
     else:
+        # Creating deep convolutional neural network architecture
         model = keras.models.Sequential([
             keras.layers.Input(shape=(96, 96, 1 if COLOR_MODE == "grayscale" else 3)),
-            keras.layers.Conv2D(16, (3, 3), activation="relu"),
-            keras.layers.MaxPooling2D(),
             keras.layers.Conv2D(64, (3, 3), activation="relu"),
             keras.layers.MaxPooling2D(),
             keras.layers.Conv2D(128, (3, 3), activation="relu"),
             keras.layers.MaxPooling2D(),
-            keras.layers.Conv2D(128, (3, 3), activation="relu", padding="same"),
+            keras.layers.Conv2D(128, (3, 3), activation="relu"),
             keras.layers.MaxPooling2D(),
             keras.layers.Conv2D(256, (3, 3), activation="relu"),
-            keras.layers.GlobalAveragePooling2D(),
-            keras.layers.Dense(512, activation="elu", kernel_initializer="lecun_normal",
-                               kernel_regularizer=keras.regularizers.l2(0.016)),
-            keras.layers.Dropout(0.3),
+            keras.layers.MaxPooling2D(),
+            keras.layers.Flatten(),
+            keras.layers.Dense(128, activation="selu"),
             keras.layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001),
-            keras.layers.Dense(256, activation="elu", kernel_initializer="lecun_normal",
-                               kernel_regularizer=keras.regularizers.l2(0.016)),
+            keras.layers.Dense(128, activation="selu"),
+            keras.layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001),
+            keras.layers.Dense(128, activation="selu"),
+            keras.layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001),
+            keras.layers.Dense(128, activation="selu"),
             keras.layers.Dense(NUM_CLASSES, activation="softmax")
         ])
 
-    model.compile(optimizer=keras.optimizers.Adamax(learning_rate=3e-3),
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+        # Compiling and setting the model
+        model.compile(optimizer=keras.optimizers.Adamax(learning_rate=3e-3),
+                      loss='categorical_crossentropy',
+                      metrics=['f1_score'])
+
+        history = model.fit(
+            train_generator,
+            epochs=EPOCHS,
+            validation_data=validation_generator,
+            callbacks=[
+                SentimentAnalyserCallback(SAVE_MODEL)
+            ]
+        )
+
+        if SHOW_TRAINING_STATISTICS:
+            # Showing statistics about training and the model
+            plot_loss_accuracy(history, SAVE_MODEL)
 
     print(model.summary())
 
-    history = model.fit(
-        train_generator,
-        epochs=EPOCHS,
-        validation_data=validation_generator,
-        callbacks=[
-            SentimentAnalyserCallback(SAVE_MODEL)
-        ]
+    # Testing the model by dummy generator
+    test_generator = datagen.flow_from_directory(
+        BASE_DIR,
+        batch_size=BATCH_SIZE,
+        class_mode="categorical",
+        target_size=IMAGE_SIZE,
+        shuffle=False,
+        subset="validation",
+        color_mode=COLOR_MODE
     )
-
-    if SHOW_TRAINING_STATISTICS:
-        plot_loss_accuracy(history, SAVE_MODEL)
-
-    # loss, acc = model.evaluate(test_generator)
-    # print(acc)
+    plot_confusion_matrix(model, test_generator)
